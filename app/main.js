43ccc513e53aa23fdfda994785d3d7a6c08afcec
Andrew8bit <Andrewbith@gmail.com>
2021-01-25 10:07:51 -0800

    changed layout, possible direction of the game will make the player movable instead of having clickable logic. idea is to create falling objects and when the player collects the objects, the player's stats will be effected.

--- app/main.js
+++ app/main.js
@@ -1,10 +1,15 @@
 const gameScreen = document.querySelector("#gameScreen");
-gameScreen.width = window.innerWidth;
-gameScreen.height = window.innerHeight;
+gameScreen.width = 800;
+gameScreen.height = 500;
 const ctx = gameScreen.getContext('2d');
 
 // rect (x , y, width, height)
 ctx.beginPath();
 ctx.rect(0,0,gameScreen.width, gameScreen.height);
+ctx.fillStyle = "#FFB26B"
 ctx.fill();
+ctx.closePath()
+
+// // placeholder character 
+
 
